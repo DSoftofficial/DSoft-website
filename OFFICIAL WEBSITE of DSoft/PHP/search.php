82301209143161cf2397f355e2df0bd0e50d96c5
DSoft <108980710+dhrubajyotifounderofDSoft@users.noreply.github.com>
2022-11-22 22:18:10 +0530

    Add files via upload

---
+++ OFFICIAL WEBSITE of DSoft/PHP/search.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>search</title>
+</head>
+<body>
+    <?php
+        echo "PHP print by echo command"
+    ?>
+</body>
+</html>
---
+++ OFFICIAL WEBSITE of DSoft/Welcome-warningJS/wlcomewarn.js
@@ -0,0 +1,2 @@
+console.log("Welcome to DSoft official website!");
+console.warn("Do Not REMOVE,EDIT, COPY, OR CLONE the *root folder or the .json* files");
